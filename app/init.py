7a2b943322cee898f851c24eca2235dd7ded36b2
Zintoulou <34sousou34@gmail.com>
2023-11-24 12:21:40 +0100

    final presentation

--- app/init.py
+++ app/init.py
@@ -6,6 +6,6 @@ import re
 from nltk.corpus import stopwords
 from nltk.stem import WordNetLemmatizer
 embedding_model = fasttext.load_model('model/fasttext.bin')
-with open('model/random_forest.pkl', 'rb') as fichier:
+with open('model/random_forest_bigdataset.pkl', 'rb') as fichier:
     best_model = pickle.load(fichier)
 lemmatizer = WordNetLemmatizer()
